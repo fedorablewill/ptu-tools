3b466012846f22790d34967130cfad41148a4d03
Will Stephenson <absorr@live.com>
2024-05-21 11:38:33 -0400

    #66 Generator - Generate from Homebrew JSON Documents

--- src/main/kotlin/com/willstep/ptutools/generator/GeneratorController.kt
+++ src/main/kotlin/com/willstep/ptutools/generator/GeneratorController.kt
@@ -7,11 +7,14 @@ import com.willstep.ptutools.dataaccess.dto.PokedexEntry
 import com.willstep.ptutools.dataaccess.dto.Pokemon
 import com.willstep.ptutools.dataaccess.service.FirestoreService
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 import org.thymeleaf.TemplateEngine
 import org.thymeleaf.context.Context
+import java.io.Serializable
 import kotlin.random.Random
+import kotlin.reflect.full.memberProperties
 
 
 @RestController
@@ -28,7 +31,7 @@ class GeneratorController {
         val nature: String?,
         val shinyOdds: Double = 0.0,
         val pokedex: String? = "1.05",
-        val pokedexEntry: PokedexEntry? = null
+        val homebrewPokedex: List<PokedexEntry> = ArrayList()
     )
 
     data class GeneratorRequestParam(
@@ -41,8 +44,48 @@ class GeneratorController {
         EQUALS, EQUALS_LIST, ARRAY_CONTAINS, ARRAY_CONTAINS_LIST
     }
 
+    @PostMapping("/validatePokedex")
+    fun validatePokedex(@RequestBody pokedexList: List<PokedexEntry>) : ResponseEntity<String> {
+        val validBaseStats = setOf("hp", "atk", "def", "spatk", "spdef", "spd")
+        val errors: MutableList<String> = ArrayList()
+        for (pokedexEntry in pokedexList) {
+            if (pokedexEntry.species.isNullOrEmpty()) {
+                errors.add("'species' is required")
+            }
+            if (pokedexEntry.types.isEmpty()) {
+                errors.add("'types' is required")
+            }
+            if (pokedexEntry.baseStats.isEmpty()) {
+                errors.add("'baseStats' is required")
+            } else if (pokedexEntry.baseStats.keys.toSet() != validBaseStats) {
+                errors.add("'hp', 'atk', 'def', 'spatk', 'spdef', 'spd' are required for 'baseStats'")
+            }
+            // if not genderless, male and female percent are required
+            if (!pokedexEntry.genderless && pokedexEntry.malePercent == null) {
+                errors.add("'malePercent' is required if 'genderless' is false")
+            }
+            if (!pokedexEntry.genderless && pokedexEntry.femalePercent == null) {
+                errors.add("'femalePercent' is required if 'genderless' is false")
+            }
+            if (pokedexEntry.moveLearnset == null) {
+                errors.add("'moveLearnset' is required")
+            } else if (pokedexEntry.moveLearnset!!.levelUpMoves.isEmpty()) {
+                errors.add("'moveLearnset.levelUpMoves' is required")
+            }
+            if (pokedexEntry.abilityLearnset.basicAbilities.isEmpty()) {
+                errors.add("'abilityLearnset.basicAbilities' is required")
+            }
+        }
+
+        return if (errors.isEmpty()) {
+            ResponseEntity.ok("valid")
+        } else {
+            ResponseEntity.badRequest().body(errors.joinToString("\n"))
+        }
+    }
+
     @PostMapping("/generatePokemon")
-    fun generatePokemon(@RequestBody requestBody: GeneratorRequest) : List<Pokemon> {
+    fun generatePokemon(@RequestBody requestBody: GeneratorRequest) : ResponseEntity<out Serializable> {
         var query = FirestoreService().getCollection("pokedexEntries").offset(0)
 
         if (requestBody.pokedex != null) {
@@ -67,12 +110,32 @@ class GeneratorController {
         }
 
         val results = query.get().get().filter { doFilter(it, requestBody.params) }
+        val allResults: MutableList<Any> = ArrayList(results)
+
+        if (!requestBody.homebrewPokedex.isEmpty()) {
+            // Validate Homebrew
+            val validation = validatePokedex(requestBody.homebrewPokedex)
+            if (validation.statusCode != HttpStatus.OK) {
+                return validation
+            }
+
+            // Filter the entries
+            try {
+                allResults.addAll(requestBody.homebrewPokedex.filter { doFilter(it, requestBody.params) })
+            } catch (e: IndexOutOfBoundsException) {
+                return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Invalid filter field")
+            }
+        }
+
         val pokemon = ArrayList<Pokemon>()
-        if (results.isNotEmpty()) {
+        if (allResults.isNotEmpty()) {
             for (i in 1..requestBody.count) {
+                val randPokemon = allResults[Random.nextInt(0, allResults.size)]
+                val entry = if (randPokemon is QueryDocumentSnapshot) randPokemon.toObject(PokedexEntry::class.java)
+                            else randPokemon
                 pokemon.add(
                     GeneratorService().generatePokemon(
-                        results[Random.nextInt(0, results.size)].toObject(PokedexEntry::class.java),
+                        entry as PokedexEntry,
                         level,
                         nature,
                         requestBody.shinyOdds
@@ -81,22 +144,27 @@ class GeneratorController {
             }
         }
 
-        return pokemon
+        return ResponseEntity(pokemon, HttpStatus.OK)
     }
 
     @PostMapping("/generatePokemonForModal")
-    fun generatePokemonForModal(@RequestBody requestBody: GeneratorRequest): ResponseEntity<String> {
+    fun generatePokemonForModal(@RequestBody requestBody: GeneratorRequest): ResponseEntity<out Serializable> {
         val results = generatePokemon(requestBody)
 
-        if (results.isEmpty()) {
-            return ResponseEntity.ok("<p class=\"text-center\">No Pokemon could be found that matches your filters.</p>")
+        if (results.statusCode == HttpStatus.OK) {
+            val resultEntries = results.body as List<*>
+            if (resultEntries.isEmpty()) {
+                return ResponseEntity.ok("<p class=\"text-center\">No Pokemon could be found that matches your filters.</p>")
+            }
+
+            val context = Context()
+            context.setVariable("results", resultEntries)
+            val fragmentsSelectors: Set<String> = setOf("generatorResults")
+
+            return ResponseEntity.ok(htmlTemplateEngine.process("fragments/generatorFragments", fragmentsSelectors, context))
+        } else {
+            return results
         }
-
-        val context = Context()
-        context.setVariable("results", results)
-        val fragmentsSelectors: Set<String> = setOf("generatorResults")
-
-        return ResponseEntity.ok(htmlTemplateEngine.process("fragments/generatorFragments", fragmentsSelectors, context))
     }
 
     @PostMapping("/generatePokemonByPokedexEntry")
@@ -106,7 +174,7 @@ class GeneratorController {
             nature = Nature.valueOf(requestBody.nature.toUpperCase())
         }
 
-        return GeneratorService().generatePokemon(requestBody.pokedexEntry!!, requestBody.minLevel, requestBody.maxLevel, nature, requestBody.shinyOdds)
+        return GeneratorService().generatePokemon(requestBody.homebrewPokedex[0], requestBody.minLevel, requestBody.maxLevel, nature, requestBody.shinyOdds)
     }
 
     @GetMapping("/speciesOptions")
@@ -158,6 +226,26 @@ class GeneratorController {
                     if (!(obj[requestParam.field!!] as List<Any>).contains(requestParam.value)) return false
                 RequestMethod.ARRAY_CONTAINS_LIST ->
                     if (!(obj[requestParam.field!!] as List<Any>).containsAll(parseList(requestParam.value as List<Any>))) return false
+
+                else -> return true
+            }
+        }
+        return true
+    }
+
+    fun doFilter(obj: PokedexEntry, filters: List<GeneratorRequestParam>): Boolean {
+        for (requestParam in filters) {
+            val method = obj::class.memberProperties.filter { it.name == requestParam.field }.get(0)
+
+            when (requestParam.method) {
+                RequestMethod.EQUALS ->
+                    if (requestParam.value.toString() != method.call(obj).toString()) return false
+                RequestMethod.EQUALS_LIST ->
+                    if (!parseList(requestParam.value as List<Any>).contains(method.call(obj))) return false
+                RequestMethod.ARRAY_CONTAINS ->
+                    if (!(method.call(obj) as List<Any>).contains(requestParam.value)) return false
+                RequestMethod.ARRAY_CONTAINS_LIST ->
+                    if (!(method.call(obj) as List<Any>).containsAll(parseList(requestParam.value as List<Any>))) return false
 
                 else -> return true
             }
